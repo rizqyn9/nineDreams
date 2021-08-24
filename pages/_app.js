0c255089cd23098c182f80fdadf4a4895810f9d4
rizqyn9 <rizqynugroho9@gmail.com>
2021-08-24 11:04:33 +0700

    add meta

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,17 @@ import Container from '../components/container/container'
 import { useState, useEffect } from 'react';
 import {Provider} from 'jotai'
 import NavSide from '../components/Header/Nav-Side';
+import Head from 'next/head'
+import * as Next from "next"
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
         <Container >
           <NavSide/>
           <Component {...pageProps} />
